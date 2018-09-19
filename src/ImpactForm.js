a1a3b975a3f462267f49508f8576cc9b7d97ecfe
Marijn Koolen <marijn.koolen@gmail.com>
2018-09-19 21:35:21 +0200

    Add view switching

--- src/ImpactForm.js
+++ src/ImpactForm.js
@@ -6,6 +6,9 @@ import ReactDOM from 'react-dom';
 import FormActions from './formActions.js';
 import AnnotatorInfo from './AnnotatorInfo.js';
 import DoneButton from './DoneButton.js';
+import Readme from './Readme.js';
+import Questionnaire from './Questionnaire.js';
+import ThankYou from './ThankYou.js';
 import SentenceQuestions from './SentenceQuestions.js';
 import SentenceAPI from './sentenceAPI.js';
 import AppFormStore from './formStore.js';
@@ -13,76 +16,67 @@ import AppFormStore from './formStore.js';
 class ImpactForm extends Component {
     constructor(props) {
         super(props);
+        this.changeView = this.changeView.bind(this);
         this.state = {
-            sentences: null,
-            progress: null
+            view: "readme"
         }
     }
 
     componentDidMount() {
         FormActions.setSentenceServer(this.props.apiUrl);
-        AppFormStore.bind('load-sentences', this.setSentences.bind(this));
-        AppFormStore.bind('load-progress', this.setProgress.bind(this));
-        let annotator = localStorage.getItem("annotator");
-        FormActions.loadSentences(annotator);
+        this.setView();
     }
 
-    setSentences(sentences, responses) {
-        this.setState({
-            sentences: sentences,
-            responses: responses
-        });
-        let annotator = localStorage.getItem("annotator");
-        FormActions.loadProgress(annotator);
+    changeView(view) {
+        console.log("view:", view);
+        window.localStorage.setItem("view", view);
+        this.setState({view: view});
     }
 
-    setProgress(progress) {
-        this.setState({progress: progress});
+    setView() {
+        let view = window.localStorage.getItem("view");
+        if (view)
+            this.setState({view: view});
     }
 
     render() {
-        let sentenceBlocks = null;
-        let progressBar = null;
-        if (this.state.sentences) {
-            sentenceBlocks = this.state.sentences.map((sentence, index) => {
-                sentence.number = index+1;
-                var response = null;
-                if (this.state.responses.hasOwnProperty(sentence.sentence_id))
-                    response = this.state.responses[sentence.sentence_id];
-                return (
-                    <SentenceQuestions
-                        key={sentence.number}
-                        sentence={sentence}
-                        response={response}
-                    />
-                )
-            });
-        }
+        let questionnaire = (
+            <Questionnaire changeView={this.changeView.bind(this)}/>
+        );
 
-        let makeProgressBar = (progress) => {
-            return (
-                <div className="header progress">{progress.sentences_done} van {progress.sentences_total} zinnen gedaan.</div>
-            )
-        }
-        if (this.state.progress) {
-            progressBar = makeProgressBar(this.state.progress);
-        }
+        let readme = (
+            <div className="col-md-10">
+                <Readme changeView={this.changeView.bind(this)}/>
+            </div>
+        )
+
+        let thankyou = (
+            <div className="col-md-10">
+                <ThankYou changeView={this.changeView.bind(this)}/>
+            </div>
+        )
+
+        console.log(this.state.view);
+        var view = null;
+        if (this.state.view === "readme")
+            view = readme;
+        else if (this.state.view === "questionnaire")
+            view = questionnaire;
+        else if (this.state.view === "thankyou")
+            view = thankyou;
 
         return (
-            <div className="row">
+            <div className="row top">
                 <div className="col-md-2">
+                    <div className="logo">
+                        <img src="https://huc.jauco.nl/wp-content/themes/huc2018/images/logo-knaw-humanities-cluster.png"  className="logo" />
+                    </div>
+                    <div className="logo">
+                        <img src="/images/huygens_ing.jpg"  className="logo" />
+                    </div>
                 </div>
                 <div className="col-md-10">
-                    <div className="row header">
-                        <AnnotatorInfo/>
-                        {progressBar}
-                    </div>
-                    <div className="row">
-                        {sentenceBlocks}
-                    </div>
-                    <div>
-                        <DoneButton/>
-                    </div>
+                    {view}
                 </div>
             </div>
         )
