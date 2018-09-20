b27bb6e116677b9b99557047d7c5c3374cf3aab0
Marijn Koolen <marijn.koolen@gmail.com>
2018-09-20 10:02:02 +0200

    Move config to external file, extend progress info

--- indexer.py
+++ indexer.py
@@ -1,21 +1,22 @@
 from elasticsearch import Elasticsearch
 
-NUM_ANNOTATORS = 3
-
 class Indexer(object):
 
-    def __init__(self, index="reading_impact_questionnaire", doc_type="response"):
-        self.index = index
-        self.doc_type = doc_type
-        self.es = Elasticsearch()
+    def __init__(self, config):
+        self.index = config["es_index"]
+        self.doc_type = config["es_doc_type"]
+        self.es = Elasticsearch([config["es_host"]], port=config["es_port"])
+        self.NUM_ANNOTATORS = config["num_annotators"]
+        self.num_sentences_per_page = config["num_sentences_per_page"]
 
     def reset_response_index(self):
         if self.es.indices.exists(index=self.index):
+            print("removing index", self.index)
             self.es.indices.delete(index=self.index)
 
     def index_sentences(self, sentences):
         for sentence in sentences:
-            sentence["response_status"] = "todo"
+            sentence["annotation_status"] = "todo"
             sentence["annotations"] = []
             self.index_sentence(sentence)
         self.refresh()
@@ -23,45 +24,60 @@ class Indexer(object):
     def index_sentence(self, sentence):
         self.es.index(index=self.index, doc_type=self.doc_type, id=sentence["sentence_id"], body=sentence)
 
-    def get_unfinished_sentences(self, annotator, size=10):
-        response = self.search_unfinished_sentences(annotator, size)
+    def get_unfinished_sentences(self, annotator):
+        response = self.search_unfinished_sentences(annotator)
         if response['hits']['total'] == 0:
             return []
         else:
             return [hit["_source"] for hit in response['hits']['hits']]
 
-    def search_unfinished_sentences(self, annotator, size):
+    def search_unfinished_sentences(self, annotator):
         query = {
             "query": {
                 "bool": {
                     "should": [
-                        { "match": { "response_status": "in_progress" } },
-                        { "match": { "response_status": "todo" } }
+                        { "match": { "annotation_status": "in_progress" } },
+                        { "match": { "annotation_status": "todo" } }
                     ],
                     "must_not": {
                         "match": {"annotations.annotator": annotator}
                     }
                 }
             },
-            "size": size
+            "size": self.num_sentences_per_page
         }
         return self.es.search(index=self.index, doc_type=self.doc_type, body=query)
 
-    def get_sentences_by_status(self, response_status):
-        print(response_status)
+    def get_sentences_by_status(self, annotation_status):
+        print(annotation_status)
         query = {
             "query": {
-                "match": { "response_status": response_status }
-            }
+                "match": { "annotation_status": annotation_status }
+            },
+            "size": 0
+        }
+        return self.es.search(index=self.index, doc_type=self.doc_type, body=query)
+
+    def get_sentences_by_annotator(self, annotator):
+        print(annotator)
+        query = {
+            "query": {
+                "match": { "annotations.annotator": annotator }
+            },
+            "size": 0
         }
         return self.es.search(index=self.index, doc_type=self.doc_type, body=query)
 
     def get_progress(self, annotator):
-        response_all = self.es.search(index=self.index, doc_type=self.doc_type, body={})
+        response_all = self.es.search(index=self.index, doc_type=self.doc_type, body={"size":0})
         response_done = self.get_sentences_by_status("done")
+        response_in_progress = self.get_sentences_by_status("in_progress")
+        response_done_by_you = self.get_sentences_by_annotator(annotator)
         return {
             "sentences_total": response_all['hits']['total'],
-            "sentences_done": response_done['hits']['total']
+            "sentences_done": response_done['hits']['total'],
+            "sentences_in_progress": response_in_progress['hits']['total'],
+            "sentences_done_by_you": response_done_by_you['hits']['total'],
         }
 
     def get_sentence(self, sentence_id):
@@ -74,10 +90,10 @@ class Indexer(object):
             if annotation["annotator"] == response["annotator"]:
                 sentence["annotations"].pop(annotation_index)
         sentence["annotations"].append(response)
-        if len(sentence["annotations"]) == NUM_ANNOTATORS:
-            sentence["response_status"] = "done"
+        if len(sentence["annotations"]) == self.NUM_ANNOTATORS:
+            sentence["annotation_status"] = "done"
         else:
-            sentence["response_status"] = "in_progress"
+            sentence["annotation_status"] = "in_progress"
         self.index_sentence(sentence)
 
     def refresh(self):
---
+++ settings_example.py
@@ -0,0 +1,9 @@
+config = {
+    "es_host": "localhost",
+    "es_port": "9200",
+    "es_index": "index_name",
+    "es_doc_type": "doc_type_name",
+    "api_url": "http://localhost:3001/api/reading_impact",
+    "num_sentences_per_page": 1,
+    "num_annotators": 3,
+}
